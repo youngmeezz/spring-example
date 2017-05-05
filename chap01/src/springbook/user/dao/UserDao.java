67a240f7c8c6af4cdcd1b9e027ab9d36f25f6738
zaccoding <zaccoding725@gmail.com>
2017-05-05 13:17:43 +0900

    - 1.7 의존관계 주입 완료 - 1.8 XML을 이용한 설정 노트 중

--- chap01/src/springbook/user/dao/UserDao.java
+++ chap01/src/springbook/user/dao/UserDao.java
@@ -13,10 +13,10 @@ public class UserDao {
 	
 	private ConnectionMaker connectionMaker;
 	
-	public UserDao(ConnectionMaker connectionMaker) {
+	public void setConnectionMaker(ConnectionMaker connectionMaker) {
 		this.connectionMaker = connectionMaker;
 	}
-	
+
 	public void add(User user) throws ClassNotFoundException, SQLException {
 		Connection conn = connectionMaker.makeConnection();
 		
